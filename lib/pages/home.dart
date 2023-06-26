a7952060d04a607d256e7ec47fca9e8bb9d80606
yuvisingh01 <yuvsingh2018@gmail.com>
2023-06-26 23:59:24 +0530

    Day4 completed

---
+++ lib/pages/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Welcome to the flutter app'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
